c142484275e70b787cc90f3df2c0f67bfc513f2f
rainbow954 <rainbow954@163.com>
2018-03-14 15:09:58 +0800

    adjust runzk.sh

--- runZk.sh
+++ runZk.sh
@@ -7,8 +7,8 @@ sudo docker run -d \
 -e SERVERS=172.19.0.21,172.19.0.22,172.19.0.23 \
 -e ZOODATADIR=/var/zookeeper/data \
 -e ZOODATALOGDIR=/var/zookeeper/log \
--v /home/ryan/data/dockerHub/zk-data/log:/var/zookeeper/log \
--v /home/ryan/data/dockerHub/zk-data/data:/var/zookeeper/data \
+-v /data/docker/zk-data/21/log:/var/zookeeper/log \
+-v /data/docker/zk-data/21/data:/var/zookeeper/data \
 --name=MyZookeeper1  --net shadownet --ip 172.19.0.21  rainbow954/docker-zk 
 
 sudo docker run -d \
@@ -17,12 +17,20 @@ sudo docker run -d \
 -e JMXSSL=false \
 -e JMXAUTH=false -e JMXLOG4J=true  \
 -e SERVERS=172.19.0.21,172.19.0.22,172.19.0.23 \
---name=MyZookeeper2  --net shadownet --ip 172.19.0.22  --restart=always rainbow954/docker-zk 
+-e ZOODATADIR=/var/zookeeper/data \
+-e ZOODATALOGDIR=/var/zookeeper/log \
+-v /data/docker/zk-data/22/log:/var/zookeeper/log \
+-v /data/docker/zk-data/22/data:/var/zookeeper/data \
+--name=MyZookeeper2  --net shadownet --ip 172.19.0.22  rainbow954/docker-zk 
 
 sudo docker run -d \
 -e MYID=3 \
 -e JMXPORT=8989 \
 -e JMXSSL=false \
 -e JMXAUTH=false -e JMXLOG4J=true  \
+-e ZOODATADIR=/var/zookeeper/data \
+-e ZOODATALOGDIR=/var/zookeeper/log \
+-v /data/docker/zk-data/23/log:/var/zookeeper/log \
+-v /data/docker/zk-data/23/data:/var/zookeeper/data \
 -e SERVERS=172.19.0.21,172.19.0.22,172.19.0.23 \
---name=MyZookeeper3  --net shadownet --ip 172.19.0.23  --restart=always rainbow954/docker-zk 
+--name=MyZookeeper3  --net shadownet --ip 172.19.0.23  rainbow954/docker-zk 
